00d6d226d0e4977e31a7d8acd59e59c5fa0f15dc
lingoudappatil <136546601+lingoudappatil@users.noreply.github.com>
2025-02-18 22:20:00 +0530

    b

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,8 +3,8 @@ import React from "react";
 const HomePage = () => {
   return (
     <div style={styles.container}>
-      <h2>Welcome to Home Page!</h2>
-      <p>You have successfully logged in.</p>
+      <h2>Welcome to Home Page! Created by Lingouda</h2>
+      <p>You have Successfully logged in.</p>
     </div>
   );
 };
