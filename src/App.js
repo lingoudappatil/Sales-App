98b777cd1efd71b9b455ad9d692abe055dd0d1be
lingoudappatil <136546601+lingoudappatil@users.noreply.github.com>
2025-03-02 13:43:52 +0530

    login_backend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
-import LoginPage from "./LoginPage";
-import RegisterPage from "./RegisterPage";
-import HomePage from "./HomePage";
+import React, { useState } from 'react';
+import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
+import HomePage from './HomePage';
 
-const App = () => {
-  const [currentPage, setCurrentPage] = useState("login"); // Track current page
-
-  const goToRegister = () => setCurrentPage("register");
-  const goToLogin = () => setCurrentPage("login");
-  const goToHome = () => setCurrentPage("home");
+function App() {
+  const [currentPage, setCurrentPage] = useState('login');
 
   return (
     <div>
-      {currentPage === "login" && <LoginPage goToRegister={goToRegister} goToHome={goToHome} />}
-      {currentPage === "register" && <RegisterPage goToLogin={goToLogin} goToHome={goToHome} />}
-      {currentPage === "home" && <HomePage setCurrentPage={setCurrentPage} />}
+      {currentPage === 'login' && (
+        <LoginPage 
+          goToRegister={() => setCurrentPage('register')}
+          goToHome={() => setCurrentPage('home')}
+        />
+      )}
+      
+      {currentPage === 'register' && (
+        <RegisterPage
+          goToLogin={() => setCurrentPage('login')}
+          goToHome={() => setCurrentPage('home')}
+        />
+      )}
+      
+      {currentPage === 'home' && <HomePage />}
     </div>
   );
-};
+}
 
 export default App;
